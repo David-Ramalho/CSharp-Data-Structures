d0abc8df822a01cc5f4bcac444166b1d30b6a62b
David-Ramalho <86934295+David-Ramalho@users.noreply.github.com>
2022-08-17 19:39:26 -0300

    Vet.Class
    Some studies

---
+++ Vetores/VeT_class/VetClass/Produto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VetClass
+{
+    internal class Produto
+    {
+        public double Price { get; private set; }
+        public string Name { get; private set; }
+        public int Cod { get; private set; }
+
+
+        public Produto(String name, double price)
+        {
+            Name = name;
+            Price = price;
+            Random numAleatorio = new Random();
+            Cod = numAleatorio.Next();
+
+        }
+
+
+    }
+}
---
+++ Vetores/VeT_class/VetClass/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.Intrinsics.X86;
+using VetClass;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cadastro de produtos da Loja Ramalho & CIA");
+            Console.WriteLine("");
+            Console.WriteLine("Quantos Produtos serão cadastrados?");
+            int n = int.Parse(Console.ReadLine());
+
+            //Instanciação do vetor/classe
+            Produto[] produto = new Produto[n];
+            double sum = 0.0;
+
+
+            for (int i = 0; i < n; i++)
+            {
+               int j = 1; j += i;
+               
+                Console.Clear();
+                Console.WriteLine("Qual o nome do produto " +j +"?");
+                string name = Console.ReadLine();
+                Console.WriteLine("Qual o preço do produto: " + name);
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                //Instanciação dos objetos do vetor/classe
+                produto[i] = new Produto(name, price);
+                sum += produto[i].Price;
+            }
+            Console.Clear();                       
+            Console.WriteLine("AVERAGE PRICE = " + (sum/n).ToString("F2", CultureInfo.InvariantCulture));
+
+
+
+        }
+    }
+}
