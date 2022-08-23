ef9e9ff17d416c052e3535f15f5deef446f66962
David-Ramalho <86934295+David-Ramalho@users.noreply.github.com>
2022-08-23 14:33:10 -0300

    Matrix Game!
    Game is simple, but great to practice matrices.

--- Matriz/Exercício_ Matrix game/ConsoleApp1/Program.cs
+++ Matriz/Exercício_ Matrix game/ConsoleApp1/Program.cs
@@ -13,22 +13,22 @@ namespace Course
             int j = 0;
             Console.WriteLine("                   ***Welcome to the Matrix game! Let's change reality!***\n\n");
             Console.WriteLine("Type the M for lines:");
-            int m=int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
             Console.WriteLine("Type the N for columns:");
-            int n=int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
             Console.WriteLine("Creating matrix...");
             Thread.Sleep(2000);
 
-            int[,] mat=new int[m,n];
+            int[,] mat = new int[m, n];
             Console.Clear();
             Console.WriteLine("\nNow you will add the values for each line and columns! Just type " + m + " for lines and" + n + "for columns");
-            
-            for(int i= 0; i < m; i++)
+
+            for (int i = 0; i < m; i++)
             {
                 string[] values = Console.ReadLine().Split(" ");
-                for( j=0;j<n; j++)
+                for (j = 0; j < n; j++)
                 {
-                    mat[i,j] = int.Parse(values[j]);
+                    mat[i, j] = int.Parse(values[j]);
                 }
             }
             Console.Clear();
@@ -37,17 +37,17 @@ namespace Course
             Console.Clear();
             Console.WriteLine("                   ***Welcome to the Matrix game! Let's change reality!***\n\n");
             Console.WriteLine("Matrix:\n");
-            for (int i= 0; i < m; i++)
-            {                
-                for (j=0; j<n; j++)
+            for (int i = 0; i < m; i++)
+            {
+                for (j = 0; j < n; j++)
                 {
-                    Console.Write(" "+ mat[i, j]);
+                    Console.Write(" " + mat[i, j]);
                 }
                 Console.WriteLine(" ");
             }
 
             Console.WriteLine("\nNow choose a matrix number: ");
-            int num=int.Parse(Console.ReadLine());
+            int num = int.Parse(Console.ReadLine());
 
             for (int i = 0; i < m; i++)
             {
@@ -55,11 +55,24 @@ namespace Course
                 {
                     if (num == mat[i, j])
                     {
-                        Console.WriteLine("\nPosition:\nLine "+ i+"\nColumn: "+j);
-                        Console.WriteLine("\nLeft: "+ mat[i, j-1]);
-                        Console.WriteLine("\nRight: " + mat[i, j +1]);
-                        Console.WriteLine("\nTop: " + mat[i-1, j ]);
-                        Console.WriteLine("\nBottom: " + mat[i + 1, j]);
+                        Console.WriteLine("\nPosition:\nLine " + i + "\nColumn: " + j);
+                        if (j > 0)
+                        {
+                            Console.WriteLine("\nLeft: " + mat[i, j - 1]);
+                        }
+                        if(j < m - 1)
+                        {
+                            Console.WriteLine("\nRight: " + mat[i, j + 1]);
+                        }
+                        if (i > 0)
+                        {
+                            Console.WriteLine("\nTop: " + mat[i - 1, j]);
+                        }
+                       if(i< n - 1)
+                        {
+                            Console.WriteLine("\nBottom: " + mat[i + 1, j]);
+                        }
+                        
 
                     }
                 }
